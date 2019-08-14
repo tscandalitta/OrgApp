c6a7c7e78e618f42428450f6d39f17b3daf1c104
tscandalitta <tscandalitta@gmail.com>
2019-08-14 10:48:43 -0300

    fix rangos

--- app/src/main/java/com/example/proyectoandroidtdp/GeneradorRangos/CalculadorRangos.java
+++ app/src/main/java/com/example/proyectoandroidtdp/GeneradorRangos/CalculadorRangos.java
@@ -8,9 +8,9 @@ public class CalculadorRangos implements CalculadorRangosAbstracto {
 
     public String getRangoSM(int cantidadBits){
 
-        int maximo = (int) Math.pow(2,cantidadBits)-1;
+        int maximo = (int) Math.pow(2, cantidadBits - 1) - 1;
 
-        return "(-"+maximo+", "+maximo+")";
+        return "(-" + maximo + ", " + maximo + ")";
     }
 
     public String getRangoDRC(int cantidadBits){
@@ -19,10 +19,10 @@ public class CalculadorRangos implements CalculadorRangosAbstracto {
 
     public String getRangoRC(int cantidadBits){
 
-        int maximo = (int) Math.pow(2,cantidadBits)-1;
-        int minimo = maximo+1;
+        int maximo = (int) Math.pow(2, cantidadBits - 1) - 1;
+        int minimo = maximo + 1;
 
-        return "(-"+minimo+", "+maximo+")";
+        return "(-" + minimo + ", " + maximo + ")";
     }
 
 
