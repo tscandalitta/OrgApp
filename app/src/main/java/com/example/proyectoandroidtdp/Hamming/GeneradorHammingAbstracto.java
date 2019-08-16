82724add3af69d2f319ea3bfaf05d307dc8d4632
tscandalitta <tscandalitta@gmail.com>
2019-08-16 20:13:08 -0300

    fix ayuda y strings

--- app/src/main/java/com/example/proyectoandroidtdp/Hamming/GeneradorHammingAbstracto.java
+++ app/src/main/java/com/example/proyectoandroidtdp/Hamming/GeneradorHammingAbstracto.java
@@ -1,11 +1,13 @@
 package com.example.proyectoandroidtdp.Hamming;
 
+import java.security.InvalidParameterException;
+
 public interface GeneradorHammingAbstracto {
 
     String getHamming3(String msg);
     String getHamming4(String msg);
     int[] getBitsCodigo(String msg);
     String getBitsMensaje(String msg);
-    String verificarHamming3(String msg, int politica);
-    String verificarHamming4(String msg, int politica);
+    String verificarHamming3(String msg, int politica) throws InvalidParameterException;
+    String verificarHamming4(String msg, int politica) throws InvalidParameterException ;
 }
